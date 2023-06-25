bb05fdf0336e69de709bccb94a9a2181080bd741
Pavanzinho <dms.cerberus@hotmail.com>
2023-06-25 09:09:50 -0300

    first commit

--- src/server.js
+++ src/server.js
@@ -37,5 +37,5 @@ app.use((error, request, response, next) => {
 })
 
 const PORT = process.env.PORT;
-app.listen(PORT, () =>
+app.listen(PORT,'0.0.0.0', () =>
     console.log((`Server is running at ${PORT}`)))
