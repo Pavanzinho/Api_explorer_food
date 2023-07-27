5a91f2b8c5b83f10e6fe59e112d600467a7f0232
Pavanzinho <dms.cerberus@hotmail.com>
2023-07-27 00:54:54 -0300

    fix: fix ingredients controller

--- src/controllers/IngredientsController.js
+++ src/controllers/IngredientsController.js
@@ -1,39 +1,29 @@
 const { json } = require("express");
-const knex = require("../database/knex")
+const knex = require("../database/knex");
 class IngredientsControllers {
-    async index(request, response) {
-        const user_id = request.user.id;
-        const is_admin = request.user.is_admin
+  async index(request, response) {
+    try {
+      const ingredients = await knex("ingredients").orderBy(
+        "ingredients.name",
+        "asc"
+      );
 
-        try {
-            const ingredients = await knex("ingredients")
-
-                .where(function () {
-                    if (is_admin === true) {
-                        this.where("ingredients.plate_id", user_id)
-                    }
-                })
-                .orderBy("ingredients.name", "asc")
-
-            return response.json(ingredients)
-
-
-        } catch (error) {
-            response.status(500).json({ error: "Falha na busca " });
-        }
-
+      return response.json(ingredients);
+    } catch (error) {
+      response.status(500).json({ error: "Falha na busca " });
     }
+  }
 
-    // async clientIndex(request, response) {
-    //     try {
-    //         const ingredients = await knex("ingredients")
-    //         return response.json(ingredients)
+  // async clientIndex(request, response) {
+  //     try {
+  //         const ingredients = await knex("ingredients")
+  //         return response.json(ingredients)
 
-    //     } catch (error) {
-    //         response.status(500).json({ error: "Falha ao inserir prato na tabela" });
-    //     }
+  //     } catch (error) {
+  //         response.status(500).json({ error: "Falha ao inserir prato na tabela" });
+  //     }
 
-    // }
+  // }
 }
 
 module.exports = IngredientsControllers;
